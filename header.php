7f77d8b29ffeb18b119bfbb80371c0fcd764baa0
dmitridemenkov <dmitridemenkov@gmail.com>
2026-08-21 20:40:42 +0300

    Изменил  хедер #3 исправил лого #3

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
             <div class="flex items-center justify-between">
                 <!-- Logo -->
                 <div class="flex-shrink-0">
-                    <a href="<?php echo home_url(); ?>" class="inline-block w-[122px] bg-white rounded-[8px] py-[4px] px-[8px] transition hover:-translate-y-4">
+                    <a href="<?php echo home_url(); ?>" class="inline-block w-[122px] bg-white rounded-[8px] py-[4px] px-[8px] transition hover:-translate-y-[2px]">
                         <?php if ($site['logo_id'] && $logo_url = wp_get_attachment_url($site['logo_id'])): ?>
                             <img src="<?php echo esc_url($logo_url); ?>"
                                  alt="<?php echo esc_attr($site['logo_alt']); ?>"
