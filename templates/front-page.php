a844399a8a3a5720b08d60836cc4910cd2b81fd0
dmitridemenkov <dmitridemenkov@gmail.com>
2026-08-25 11:15:50 +0300

    Наши работы кнопка padding

--- templates/front-page.php
+++ templates/front-page.php
@@ -61,9 +61,9 @@ get_header();
 <!-- Works Section -->
 <section class="mt-[36px] lg:mt-[64px]" id="works">
     <div class="container mx-auto px-3">
-        <div class="flex flex-wrap items-center justify-between gap-4 w-[100%]">
+        <div class="flex flex-wrap items-start justify-between gap-4 w-[100%]">
             <h2 class="inline-block relative mb-[24px] lg:mb-[36px] text-xl lg:text-3xl m-w-[50%]">Наши работы</h2>
-            <a href="/" class="text-xs lg:text-xl font-medium text-black border-2 border-[#20436C] rounded-[24px] lg:rounded-[44px] transition hover:text-[#20436C]" title="Подробнее">Подробнее</a>
+            <a href="/" class="text-xs lg:text-xl font-medium text-black border-2 border-[#20436C] rounded-[24px] lg:rounded-[44px] px-[16px] py-[6px] lg:px-[28px] lg:py-[12px] transition hover:text-[#20436C]" title="Подробнее">Подробнее</a>
         </div>
     </div>
 </section>
