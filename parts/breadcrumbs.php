6bf044774b629cec1f41e1b6865e4c37848d519d
dmitridemenkov <dmitridemenkov@gmail.com>
2026-08-26 12:59:57 +0300

    breadcrumbs mt

--- parts/breadcrumbs.php
+++ parts/breadcrumbs.php
@@ -6,7 +6,7 @@
 $title = $args['title'] ?? get_the_title();
 ?>
 
-<nav class="text-sm mb-6" aria-label="Хлебные крошки">
+<nav class="mt-4 lg:mt-0 text-sm mb-6" aria-label="Хлебные крошки">
     <a href="<?php echo home_url(); ?>" class="text-[#BFBFBF] hover:text-black transition" title="Главная">Главная</a>
     <span class="text-[#BFBFBF]"> / </span>
     <span class="text-black font-medium"><?php echo esc_html($title); ?></span>
