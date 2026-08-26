c9e617a199bde19a2003e53d1991c904321ee2ce
dmitridemenkov <dmitridemenkov@gmail.com>
2026-08-26 16:03:41 +0300

    Создание блока материалы карточка

--- parts/materials.php
+++ parts/materials.php
@@ -6,5 +6,30 @@
                 class="text-xs lg:text-xl font-medium text-black border-2 border-[#20436C] rounded-[24px] lg:rounded-[44px] px-[16px] py-[6px] lg:px-[32px] lg:py-[12px] transition hover:text-white hover:bg-[#20436C]"
                 title="Смотреть все отзывы">Подробнее</a>
         </div>
+        <div class="flex flex-col gap-2">
+            <div class="relative bg-[#F4F4F4] flex">
+                <div class="py-3 ps-4 lg:ps-12">
+                    <div class="text-base lg:text-2xl font-medium text-black mb-3 lg:mb-4">
+                        Премиальные ящики Avantech от Hettich
+                    </div>
+                    <div class="content text-sm lg:text-xl text-black font-regular">
+                        <ul>
+                            <li>Выполнен в стиле кухни: 120 вариантов дизайна</li>
+                            <li>Вместимость до 1,5 раз больше за счёт высоких и тонких боковин</li>
+                            <li>Бесшумное полное закрывание даже с нагрузкой до 30 кг</li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <picture>
+                        <source srcset="https://delinehome.ru/wp-content/uploads/2026/08/material_1.avif"
+                            type="image/avif">
+                        <img src="https://delinehome.ru/wp-content/uploads/2026/08/material_1.png"
+                            class="h-[100%] w-[140px] lg:w-[360px] inline-block object-cover"
+                            alt="Фото материала" title="Фото материала" loading="lazy">
+                    </picture>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
