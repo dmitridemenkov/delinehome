06c63e89f93ed046f821a045051f5315824dfc94
dmitridemenkov <dmitridemenkov@gmail.com>
2026-08-28 14:20:07 +0300

    Изменение стилей промо-блока в каталоге

--- parts/promo-card.php
+++ parts/promo-card.php
@@ -20,7 +20,7 @@ foreach ([1, 2] as $n) {
 }
 ?>
 
-<div class="promo-card p-6">
+<div class="promo-card p-6 rounded-[6px] lg:rounded-[16px]">
     <div class="promo-card__body">
         <?php if ($promo['title']): ?>
         <h3 class="font-medium font-xl lg:font-4xl text-white mb-4 lg:mb-6"><?php echo esc_html($promo['title']); ?></h3>
